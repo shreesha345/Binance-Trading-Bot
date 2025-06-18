1632a76a44feec8356f74184c873ad23f20c65bd
Shreesha345 <shreeshaaithal862@gmail.com>
2025-06-19 04:08:27 +0530

    added: a simple basic strategy which only buys for even integer/number

--- basic_strategy.py
+++ basic_strategy.py
@@ -1,67 +1,43 @@
-from utils.buy_sell_handler import create_buy_order,create_sell_order,open_orders,FUTURE_ORDER_TYPE_LIMIT,FUTURE_ORDER_TYPE_STOP
-from utils.config import BINANCE_API_KEY, BINANCE_API_SECRET, TEST
-from binance.client import Client
+def generate_signal(ohlc_data):
+    """
+    Basic strategy: Generate BUY signal when close price is an even number
+    
+    Args:
+        ohlc_data (dict): Dictionary containing OHLC data with keys:
+            - open, high, low, close (float values)
+            - symbol, time (string values)
+    
+    Returns:
+        str: 'BUY' if close price is even, 'HOLD' otherwise
+    """
+    close_price = float(ohlc_data['close'])
+    
+    # Check if the integer part of close price is even
+    if int(close_price) % 2 == 0:
+        return 'BUY'
+    else:
+        return 'HOLD'
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-symbol = "ETCUSDT"
-quantity = 1
-price= 17.26
-Trigger_price = 17.28
-# sell_price = 17.24
-client = Client(BINANCE_API_KEY, BINANCE_API_SECRET, testnet=TEST)
-
-# buy = create_buy_order(
-#     symbol=symbol,
-#     quantity=1,
-#     price=17.28,
-#     stop_price=17.25,  # Trigger price for stop-loss
-#     order_type=FUTURE_ORDER_TYPE_LIMIT,
-#     client=client
-# )
-
-# print(buy)
-
-
-# sell = create_sell_order(
-#     symbol=symbol,
-#     quantity=quantity,
-#     price=sell_price,
-#     order_type=FUTURE_ORDER_TYPE_LIMIT,
-#     client=client
-# )
-
-# print(sell)
-
-
-# orders_open = open_orders(
-#     symbol=symbol,
-# )
-
-# print(orders_open)
-
-stop_loss_order = create_sell_order(
-    symbol="ETCUSDT",
-    quantity=1,
-    price=price,  # Price at which the stop-loss order will be triggered
-    stop_price=Trigger_price,  # Trigger price for stop-loss
-    order_type=FUTURE_ORDER_TYPE_STOP,  # Or FUTURE_ORDER_TYPE_STOP_MARKET
-    client=client
-)
-
-print(stop_loss_order)
-
+def calculate_entry_sl(ohlc_data, signal):
+    """
+    Calculate entry price and stop loss based on signal
+    
+    Args:
+        ohlc_data (dict): OHLC data
+        signal (str): Trading signal ('BUY' or 'HOLD')
+    
+    Returns:
+        dict: Entry price and stop loss levels
+    """
+    if signal == 'BUY':
+        entry = ohlc_data['close']
+        stop_loss = ohlc_data['low']
+        return {
+            'entry': entry,
+            'stop_loss': stop_loss
+        }
+    else:
+        return {
+            'entry': None,
+            'stop_loss': None
+        }
