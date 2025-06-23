bcde756879bca3fa9cf97f3998c3c11f8c3c3b5f
Shreesha345 <shreeshaaithal862@gmail.com>
2025-06-24 02:28:22 +0530

    Add debug mode option to clear_screen utility
    - Added debug_mode parameter to clear_screen function
    - Modified function to skip screen clearing when debug_mode is enabled
    - Allows for better debugging by preserving terminal output when needed
    
    This enhancement makes it easier to debug the application by keeping terminal
    output visible when running in debug mode.

--- utils/websocket_client/clear_screen.py
+++ utils/websocket_client/clear_screen.py
@@ -1,5 +1,7 @@
 # Utility for clearing the terminal screen
 import os
 
-def clear_screen():
-    os.system('cls' if os.name == 'nt' else 'clear')
+def clear_screen(debug_mode=False):
+    """Clear the terminal screen unless in debug mode"""
+    if not debug_mode:
+        os.system('cls' if os.name == 'nt' else 'clear')
